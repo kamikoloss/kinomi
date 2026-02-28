95b100251eff6e2a8642efe0fe6bd6098dabd88e
Kamiko Loss <info@gloxi.net>
2026-02-28 15:00:10 +0900

    forge.config に publisher-github 用の設定を追加した

--- client/forge.config.ts
+++ client/forge.config.ts
@@ -54,6 +54,18 @@ const config: ForgeConfig = {
       [FuseV1Options.OnlyLoadAppFromAsar]: true,
     }),
   ],
+  publishers: [
+    {
+      name: '@electron-forge/publisher-github',
+      config: {
+        repository: {
+          owner: 'kamikoloss',
+          name: 'kinomi'
+        },
+        prerelease: true
+      }
+    }
+  ]
 };
 
 export default config;
